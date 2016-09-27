51e9a7f6f99bd44925aeb529d36c945bf91af408
lottiegreenwood <lottie.greenwood@stfc.ac.uk>
2016-09-27 13:53:34 +0000

    trying to figure out how to parse a reply

--- initialConvo.py
+++ initialConvo.py
@@ -7,6 +7,12 @@ BOT_ID = os.environ.get("BOT_ID")
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 userList = ""
 
+def lastMessageFromUser(im_history):
+	print im_history
+	message = 1
+	return message
+
+
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
@@ -25,6 +31,8 @@ if __name__ == "__main__":
 		else:
 		    id = clientChat[u'channel'][u'id']
 		    print slack_client.api_call("chat.postMessage", as_user="true:", channel=id, text = greeting)
+		    time.sleep(20) #need to know have recieved a reply?
+		    print lastMessageFromUser(slack_client.api_call("im.history", channel=id, count = 1))
 	    break
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
